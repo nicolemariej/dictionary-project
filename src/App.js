ecec5b571bc0dc1710fb05863c9f825ff2cc8c9c
nicolemariej <88035900+nicolemariej@users.noreply.github.com>
2021-09-04 22:33:16 +1000

    Add dictionary component and adding form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Dictionary />
+      <p>Coded by Nicole Jones</p>
+    </div>
+  );
 }
 
 export default App;
