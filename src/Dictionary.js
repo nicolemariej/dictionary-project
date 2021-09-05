c3c7e5845bb61b7810a4405a45c4374b5b89917f
nicolemariej <88035900+nicolemariej@users.noreply.github.com>
2021-09-05 13:42:55 +1000

    added prompt to h2 element

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState({});
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -27,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
