b50830ebe6f336b9a92f3508e923ce1836b9da0f
nicolemariej <88035900+nicolemariej@users.noreply.github.com>
2021-10-01 20:33:05 +1000

    Improve styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <Dictionary />
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />{" "}
+        <span className="title">Dictionary</span>
+      </header>
+      <main>
+        <Dictionary />
+      </main>
       <p>Coded by Nicole Jones</p>
     </div>
   );
---
+++ src/Phonetic.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Phonetic.css";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="blank">
+        Listen
+      </a>
+      {props.phonetic.text}
+    </div>
+  );
+}
